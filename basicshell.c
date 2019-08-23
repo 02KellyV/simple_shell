ce5c613343d3d9bf7d5be27616a0d1cc59af11f2
02KellyV <900@holbertonschool.com>
2019-08-23 02:39:49 +0000

    basicshell fix

--- basicshell.c
+++ basicshell.c
@@ -1,21 +1,39 @@
 #include "simple_shell.h"
 
 /**
-* main - Shell functions
-* @argc: The number of arguments, variable unused.
-* @argv: Arguments.
-* 
-* Return: A int value.
+ * usetok - Shell functions
+ * @buff: to save info
+ * @tok: pointer to chars array
+ * Return: A char value.
+ */
+
+void usetok(char *buff, char *tok[])
+{
+	int n = 1;
+
+	tok[0] = strtok(buff, " \n\t");
+	while (tok[n] != '\0')
+	{
+		tok[n] = strtok(NULL, " \n\t");
+		n++;
+	}
+	tok[n] = NULL;
+}
+
+/**
+* main - Contains the basic Shell functions
+* Return: void.
 */
+
 int main(void)
 {
 	char *buff, *tok[100];
 	size_t size = 0;
-	int line, exe, other, n = 1;
+	int line, exe, other;
 
 	buff = malloc(sizeof(char *) * 1024);
-	if(buff == NULL)
-		return(0);
+	if (buff == NULL)
+		return (0);
 	while (1)
 	{
 		write(1, "$ ", 2);
@@ -26,29 +44,22 @@ int main(void)
 			perror("Error");
 			break;
 		}
-		tok[0] = strtok(buff, " \n\t");
-		while(tok[n] != '\0')
-		{
-			tok[n] = strtok(NULL, " \n\t");
-			n++;
-		}
-		tok[n] = NULL;
-	/*	printf("%s\n", tok[1]); */
+		usetok(buff, tok);
 		if (fork() == 0)
 		{
-		exe = execve(tok[0], tok, NULL);
+			exe = execve(tok[0], tok, NULL);
 			if (exe == -1)
 			{
-			perror("Error");
-			free(buff);
-			exit(0);
-			}	
+				perror("Error");
+				free(buff);
+				exit(0);
+			}
 		}
 		else
 		{
 			wait(&other);
 		}
 	}
-free(buff);
-return (0);
+	free(buff);
+	return (0);
 }
