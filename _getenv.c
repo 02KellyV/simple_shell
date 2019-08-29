ccd5db510e7c3ea9fb294fa183e5c6ffe95bd723
amartinezre05 <almartinezre05@gmail.com>
2019-08-29 00:06:03 +0000

    Try to change the printf for write

--- _getenv.c
+++ _getenv.c
@@ -34,5 +34,7 @@ void printenv(void)
 	for (i = 0; environ[i] != NULL; i++)
 	{
 		printf("\n%s", environ[i]);
+		/*write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);*/
 	}
 }
