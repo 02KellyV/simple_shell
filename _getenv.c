1bbdab562bd50e0e37ce808f473d67fcc88ec25f
amartinezre05 <almartinezre05@gmail.com>
2019-08-29 04:13:29 +0000

    Changes the print function

--- _getenv.c
+++ _getenv.c
@@ -33,8 +33,8 @@ void printenv(void)
 
 	for (i = 0; environ[i] != NULL; i++)
 	{
-		printf("\n%s", environ[i]);
-		/*write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
-		write(STDOUT_FILENO, "\n", 1);*/
+		/*printf("\n%s", environ[i]);*/
+		write(STDOUT_FILENO, environ[i], _strlen(environ[i]));
+		write(STDOUT_FILENO, "\n", 1);
 	}
 }
